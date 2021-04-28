ec4aabaad75d6fc12f81c5872ee1b6d2df68283f
tomicakr <tomislav.kravarscan@axilis.com>
2021-04-28 13:28:13 +0200

    try fix 5 second sleep

--- scrapers/index.js
+++ scrapers/index.js
@@ -1,6 +1,8 @@
 const { portals } = require('./common.js');
 const { scrape } = require('./scaper.js');
 
-Object.values(portals).forEach(portal => {
-    scrape(portal);
-});
+(async () => {
+    Object.values(portals).forEach(async portal => {
+        await scrape(portal);
+    });
+})();
