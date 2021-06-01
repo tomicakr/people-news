9574a286a4b87e32e9f6d7d979e905dad0d884c8
tomicakr <kravarscan.tomica@gmail.com>
2021-06-01 18:00:36 +0200

    add very simple server

---
+++ api/db_models.py
@@ -0,0 +1,6 @@
+def to_post(db_object):
+    return {
+        'url': db_object['url'],
+        'title': db_object['title'],
+        'text': db_object['text']
+    }
---
+++ api/server.py
@@ -0,0 +1,32 @@
+from flask import request, jsonify, Flask
+from pymongo import MongoClient
+from db_models import to_post
+import flask
+
+app = Flask(__name__)
+client = MongoClient('mongodb://localhost:27017/')
+db = client.newsPosts
+posts = db.posts
+
+# get one post
+@app.route('/post/<titlehash>', methods=['GET'])
+def get_post(titlehash):
+    db_post = posts.find_one({ 'titleHash': titlehash })
+    print(db_post)
+    return jsonify(to_post(db_post))
+
+@app.route('/posts', methods=['GET'])
+def get_recent_posts():
+    count = request.args.get('count')
+    if count is None:
+        count = 10
+    count = int(count)
+
+    all_posts = posts.find()
+    all_posts = all_posts[:count]
+    posts_res = []
+    for p in all_posts:
+        posts_res.append(to_post(p))
+    return jsonify(posts_res)
+
+app.run()
