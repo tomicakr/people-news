780efa7065169af4615a25d4a2c26e1b0f8ff159
tomicakr <kravarscan.tomica@gmail.com>
2021-06-09 21:01:57 +0200

    add adding people group and getting posts which contain names

--- api/server/index.py
+++ api/server/index.py
@@ -8,11 +8,12 @@ CORS(app)
 client = MongoClient('mongodb://localhost:27017/')
 db = client.newsPosts
 posts = db.posts
+groups = db.groups
 
 # get one post
-@app.route('/post/<titlehash>', methods=['GET'])
-def get_post(titlehash):
-    db_post = posts.find_one({ 'titleHash': titlehash })
+@app.route('/post/<title_hash>', methods=['GET'])
+def get_post(title_hash):
+    db_post = posts.find_one({ 'titleHash': title_hash })
     print(db_post)
     return jsonify(to_post(db_post))
 
@@ -33,5 +34,31 @@ def get_recent_posts():
     for p in all_posts:
         posts_res.append(to_post(p))
     return jsonify(posts_res)
+
+@app.route('/postsgrouped/<group_name>', methods=['GET'])
+def get_recent_by_group(group_name):
+    count = request.args.get('count')
+    start = request.args.get('start')
+    if count is None:
+        count = 10
+    if start is None:
+        start = 0
+    count = int(count)
+    start = int(start)
+
+    group = groups.find_one({ 'groupName': group_name })
+    people = group['people']
+    relevantPosts = posts.find({"names": {"$in": people}}).sort('dateAdded', DESCENDING)
+    relevantPosts = relevantPosts[start:start+count]
+    posts_res = []
+    for p in relevantPosts:
+        posts_res.append(to_post(p))
+    return jsonify(posts_res)
+
+@app.route('/add_group', methods=['POST'])
+def add_group():
+    obj = request.get_json()
+    db.groups.insert_one(obj)
+    return "success"
 
 app.run(port=3000)
